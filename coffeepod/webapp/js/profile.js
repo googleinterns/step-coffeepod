7cb97dda431ac49ad06d158762379f5210236cb2
mariamaynard <maria.maynard213@gmail.com>
2020-07-07 18:50:42 +0000

    making page ipad friendly?

--- coffeepod/webapp/js/profile.js
+++ coffeepod/webapp/js/profile.js
@@ -2,7 +2,6 @@ var name, email, photoUrl, uid, emailVerified;
 var user = firebase.auth().currentUser;
 
 function getProfile() {
-  console.log("hello there");
   // if (user) {
   //   // User is signed in.
   //   name = user.displayName;
@@ -14,3 +13,57 @@ function getProfile() {
   //   // No user is signed in.
   // }
 }
+
+function updateAbout() {
+  document.getElementById("updateAbt").classList.add("hidden");
+  document.getElementById("saveAbt").classList.remove("hidden");
+  document.getElementById("aboutText").disabled = false;
+}
+
+function saveAbout() {
+  document.getElementById("saveAbt").classList.add("hidden");
+  document.getElementById("updateAbt").classList.remove("hidden");
+  document.getElementById("aboutText").disabled = true;
+}
+
+function updateExperience() {
+  document.getElementById("updateExp").classList.add("hidden");
+  document.getElementById("saveExp").classList.remove("hidden");
+  var elements = document.getElementsByClassName("experienceFrom");
+  for (var i = 0, len = elements.length; i < len; i++) {
+    elements[i].disabled = false;
+  }
+}
+
+function saveExperience() {
+  document.getElementById("saveExp").classList.add("hidden");
+  document.getElementById("updateExp").classList.remove("hidden");
+  var elements = document.getElementsByClassName("experienceFrom");
+  for (var i = 0, len = elements.length; i < len; i++) {
+    elements[i].disabled = true;
+  }
+}
+
+function updateEducation() {
+  document.getElementById("updateEdu").classList.add("hidden");
+  document.getElementById("saveEdu").classList.remove("hidden");
+  var elements = document.getElementsByClassName("educationForm");
+  for (var i = 0, len = elements.length; i < len; i++) {
+    elements[i].disabled = false;
+  }
+}
+
+function saveEducation() {
+  document.getElementById("saveEdu").classList.add("hidden");
+  document.getElementById("updateEdu").classList.remove("hidden");
+  var elements = document.getElementsByClassName("educationForm");
+  for (var i = 0, len = elements.length; i < len; i++) {
+    elements[i].disabled = true;
+  }
+}
+
+$(function () {
+    $("textarea").each(function () {
+        this.style.height = (this.scrollHeight+10)+'px';
+    });
+});
