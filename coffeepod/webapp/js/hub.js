3afd0cbcd9550ab8ab15c2638ff1e76f7d971a93
Phuong Nguyen <thiminhphuongnguyen00@gmail.com>
2020-07-20 18:45:05 +0000

    Get user info from firebase

--- coffeepod/webapp/js/hub.js
+++ coffeepod/webapp/js/hub.js
@@ -1,5 +1,27 @@
 // This file is to pull mentors/ mentees information onto current user's hub page
 
-function createIndCard() {
-    
+var name, photoUrl, uid, emailVerified, user;
+
+let mentorPairs, menteePairs, userId;
+
+function getProfile() {
+  auth.onAuthStateChanged(function(user) {
+    if (user) { // if user is logged in 
+      uid = user.uid;
+      var profileRef = db.collection("profile").doc(uid);
+      profileRef.get().then(function(profile) {
+        if (profile.exists) {
+          updatePage(profile);
+          console.log("Document data:", profile.data());
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such profile!");
+        }
+      }).catch(function(error) {
+        console.log("Error getting profile:", error);
+      });
+    } else {
+      console.log("not logged in");
+    }
+  });
 }
