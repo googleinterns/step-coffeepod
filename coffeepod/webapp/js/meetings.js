acd0f56771154141e8ce2e164854ebcc8e2b9fe5
Phuong Nguyen <phuongtnguyen@google.com>
2020-07-24 04:31:29 +0000

    Test firebase by adding a map to a field that has not been instantiated

--- coffeepod/webapp/js/meetings.js
+++ coffeepod/webapp/js/meetings.js
@@ -1,5 +1,31 @@
 
+
 function sendMeetingRequest(event){
     event.preventDefault();
-    console.log("I should see something now");
+    // get information from the form
+    const meetingForm = document.getElementById("meeting-form");
+    const title = meetingForm['title'].value;
+    const when = new Date(meetingForm['when'].value);
+    const where = meetingForm['where'].value;
+    const description = meetingForm['description'].value;
+
+    // add the form
+    const addConfirm = document.getElementById("add-confirm-meeting");
+    addConfirm.innerText = "Your meeting request is sent! Add another meeting:";
+}
+ 
+ testFirebase();
+ testDeleteFirebase();
+ // add to a field that has not been instantiated
+function testFirebase() {
+    console.log(mentorshipID);
+    db.collection('mentorship').doc(mentorshipID).collection('meetings').doc('iWe6BVc6KtP4yj0yw1Om').update({
+        request: firebase.firestore.FieldValue.arrayUnion({mentorshipId: mentorshipID, meetingId: 'a4KWy8d3pjbJMMsP5NBt'})
+    })
+}
+
+function testDeleteFirebase(){
+    db.collection('mentorship').doc(mentorshipID).collection('meetings').doc('iWe6BVc6KtP4yj0yw1Om').update({
+        request: firebase.firestore.FieldValue.arrayRemove({mentorshipId: mentorshipID, meetingId: 'a4KWy8d3pjbJMMsP5NBt'})
+    })
 }
