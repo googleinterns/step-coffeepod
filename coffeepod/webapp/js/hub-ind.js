ca35d325a869d13136c311511a54a51d01eb8542
Phuong Nguyen <phuongtnguyen@google.com>
2020-07-27 22:18:43 +0000

    Expand the details of each meeting in different categories

--- coffeepod/webapp/js/hub-ind.js
+++ coffeepod/webapp/js/hub-ind.js
@@ -85,12 +85,11 @@ function getAllMeetings() {
 
 function getAcceptedMeetings() {
     const meetingsRef = db.collection('mentorship').doc(mentorshipID).collection('meetings');
-    console.log("get all upcoming meetings - meetings that have been accepted");
     let meetingCount = 0;
     meetingsRef.where('when', '>', Date.now()).where('accepted', '==', true).orderBy("when", "asc").get().then(function (meetings) {
         meetings.forEach(meeting => {
             meetingCount += 1;
-            addMeetingToList("upcoming-meeting-list", meeting.data().id, meeting.data().when);
+            addMeetingToList("upcoming-meeting-list", meeting.id, meeting.data().when);
         });
             document.getElementById("num-upcoming").innerText = meetingCount;
             document.getElementById("upcoming-meeting-list").classList.remove('hidden');
@@ -99,12 +98,11 @@ function getAcceptedMeetings() {
 
 function getPendingMeetings() {
     const meetingsRef = db.collection('mentorship').doc(mentorshipID).collection('meetings');
-    console.log("get all pending meetings - meetings that have been not been accepted but are still pending");
     let meetingCount = 0;
     meetingsRef.where('when', '>', Date.now()).where('pending', '==', true).orderBy("when", "asc").get().then(function (meetings) {
         meetings.forEach(meeting => {
             meetingCount += 1;
-            addMeetingToList("pending-meeting-list", meeting.data().id, meeting.data().when);
+            addMeetingToList("pending-meeting-list", meeting.id, meeting.data().when);
         });
             document.getElementById("num-pending").innerText = meetingCount;
             document.getElementById("pending-meeting-list").classList.remove('hidden');
@@ -113,14 +111,12 @@ function getPendingMeetings() {
 
 function getPastMeetings() {
     const meetingsRef = db.collection('mentorship').doc(mentorshipID).collection('meetings');
-    console.log("get all pending meetings - meetings that have been not been accepted but are still pending");
     let meetingCount = 0;
     meetingsRef.where('when', '<=', Date.now()).where('pending', '==', false).where('accepted', '==', true).orderBy("when", "asc").get().then(function (meetings) {
         meetings.forEach(meeting => {
             meetingCount += 1;
-            addMeetingToList("past-meeting-list", meeting.data().id, meeting.data().when);
+            addMeetingToList("past-meeting-list", meeting.id, meeting.data().when);
         });
-        console.log("past meeting count: " + meetingCount);
         document.getElementById("num-past").innerText = meetingCount;
         document.getElementById("past-meeting-list").classList.remove('hidden');
     });
@@ -136,7 +132,53 @@ function addMeetingToList(listId, meetingId, meetingWhen){
 
     const meetingDate = meetingLiEleCloned.querySelector("#meeting-date");
     meetingDate.innerText = new Date(meetingWhen);
-    meetingDate.setAttribute('onclick', 'showMeetingDetail(' + meetingId + ')')
+    meetingDate.setAttribute('onclick', 'showMeetingDetails(' + "'" + meetingId + "'" + ')');
 
     meetingList.appendChild(meetingLiEleCloned);
+}
+
+function showMeetingDetails(meetingId) {
+    const mentorshipDoc =  db.collection('mentorship').doc(mentorshipID);
+    const meetingsRef = mentorshipDoc.collection('meetings');
+    const whenExpand = document.getElementById("expand-when");
+    const whereExpand = document.getElementById("expand-where");
+    const setByExpand = document.getElementById("expand-set-by");
+    const descriptionExpand = document.getElementById("expand-description");
+    const titleExpand = document.getElementById("expand-title");
+
+    meetingsRef.doc(meetingId).get().then(function(meeting) {
+        whenExpand.innerText = new Date(meeting.data().when);
+        whereExpand.innerText = meeting.data().where;
+        descriptionExpand.innerText = meeting.data().description;
+        titleExpand.innerText = meeting.data().title;
+
+        // Get from mentor - mentee name currently in the html page
+        if (meeting.data().setByMentor == true) {
+            setByExpand.innerText = document.getElementById("mentor-name").innerText;
+        } else {
+            setByExpand.innerText = document.getElementById("mentee-name").innerText;
+        }
+    })
+
+    const detailSection = document.getElementById("meeting-details-expand");
+
+    // Show the details section
+    if (detailSection.style.display == 'none') {
+        detailSection.style.display = 'block';
+
+        // Reduce the space for schedule
+        document.querySelectorAll('.tab-pane').forEach(tabPane =>
+            tabPane.style.height = '200px');
+        document.getElementById('meetings-box').style.height = '300px';
+    };
+
+}
+
+function hideDetails(){
+    const detailSection = document.getElementById("meeting-details-expand");
+    detailSection.style.display = "none";
+    // Increase the space for tab-pan
+    document.querySelectorAll('.tab-pane').forEach(tabPane =>
+            tabPane.style.height = '400px');
+    document.getElementById('meetings-box').style.height = '500px';
 }
